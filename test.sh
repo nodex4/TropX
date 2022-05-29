25dd20705a107770d5c3e52cc9b1a66e82176335
Eugene <60685856+troopek@users.noreply.github.com>
2022-05-29 10:29:36 +0000

    test

--- test.sh
+++ test.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
 
-if [[ -z $(git status -s) ]]
-then
+if [[ -z $(git status -s) ]]; then
   echo "up to date"
 else
   echo "update pls"
 fi
+
+output=$(git status -uno)
+if [[ $output == *"commit"* ]]; then
+  echo "update pls"
+else  
+  echo "uptodate"
+fi
+echo $output
