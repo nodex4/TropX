46aa96f4dc739e667c3507b31ea7afeddbb2881e
Eugene <60685856+troopek@users.noreply.github.com>
2022-05-29 08:04:01 +0000

    test

--- main.sh
+++ main.sh
@@ -838,8 +838,17 @@ fi
 
 if [[ $SS == "u" ]]; then
   :
-  
-fi
+  changed=0
+  git remote update && git status -uno | grep -q 'Your branch is behind' && changed=1
+  if [ $changed = 1 ]; then
+      echo "Not up to date";
+      read r
+      git pull
+      echo "Updated successfully";
+  else
+      echo "Up-to-date"
+  fi
+  fi
 
 
 
