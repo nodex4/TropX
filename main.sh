651365484dc6e01283f739d64ffb86cad4002ff0
Eugene <60685856+troopek@users.noreply.github.com>
2022-05-29 19:03:01 +0000

    added checkSettings for WI

--- main.sh
+++ main.sh
@@ -25,6 +25,9 @@ function checkSettings() {
 ################################################################################################################
 
 function ready() {
+  checkSettings "Wireless Interface"
+  WI=$value
+
   checkSettings "Primary Color"
   
   primary=$value
