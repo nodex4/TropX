0a4643c8e6aba8a0e375557d0d23572b1131abc2
Eugene <60685856+troopek@users.noreply.github.com>
2022-06-12 18:15:34 +0000

    bug fix

--- setup.sh
+++ setup.sh
@@ -104,10 +104,21 @@ if [[ $ans == "y" ]]; then
 Type=Application
 Terminal=true
 Name=TropX
+Comment=Run TropX
 Icon="$directory"/images/logo.png
 Exec=bash -c 'cd "$directory"; bash "$directory"/main.sh'" > "$desktop/TropX.desktop"
-  chmod +x "$desktop/TropX.desktop"
-  gio set "$desktop/TropX.desktop" metadata::trusted true
+
+  echo -e "$PRIMARY  What is the directory of where you want the shortcut saved? (${SECONDARY}specify full path!${PRIMARY})$SECONDARY"
+  read desktop
+  echo "[Desktop Entry]
+Type=Application
+Terminal=true
+Name=TropX
+Comment=Run TropX
+Icon="$directory"/images/logo.png
+Exec=bash -c 'cd "$directory"; bash "$directory"/main.sh'" > /usr/share/applications/
+  chmod +x "/usr/share/applications//TropX.desktop"
+  gio set "/usr/share/applications//TropX.desktop" metadata::trusted true
 fi
 
 clear
