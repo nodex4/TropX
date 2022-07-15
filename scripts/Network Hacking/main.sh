e41870ceb1ff25cca7ccaa7861a3453294bd9dc8
Eugene <60685856+troopek@users.noreply.github.com>
2022-07-15 12:43:32 +0000

    bug fix

--- scripts/Network Hacking/main.sh
+++ scripts/Network Hacking/main.sh
@@ -11,23 +11,24 @@ ready
 selectOptions "Manage Mac & IP" "Selection" "Try another selection" "Wifi Cracking" "AP Spam"
 if [[ "$SO" == "1" ]]; then   # Wifi Cracking
   :
+
 elif [[ "$SO" == "2" ]]; then # AP Spam
   selectOptions "AP Spam" "Selection" "Try another selection" "Random Network Names" "Network Names from Worldist" "Specific Network Names"
   if [[ "$SO" == "1" ]]; then   # Random
     # getInput "Number of APs" "Enter the number of Access Points to generate." "1000"
     changeWImode monitor
     changeMac
-    mdk3 $WI b -f ./random_wordlist.txt -a # -s $N
+    mdk3 $WI b -f ./random_wordlist.txt -a -s 1000
   elif [[ "$SO" == "2" ]]; then # Wordlist
     getInput "Wordlist File" "Enter Wordlist Path." "/root/desktop/wordlist.txt"
     changeWImode monitor
     changeMac
-    mdk3 $WI b -f "$SI" -a -s $(wc -l "$SI" | cut -f1 -d ' ')
+    mdk3 $WI b -f "$SI" -a -s 1000
   elif [[ "$SO" == "3" ]]; then # Specific
     getInput "Network Name" "Type a string" "xPWNEDx"
     string=$SI
     getInput "Number of APs" "Enter the number of Access Points to generate." "1000"
     N=$SI
-    mdk3 $WI b -n "$string" -a
+    mdk3 $WI b -n "$string" -a -s $N
   fi
 fi
